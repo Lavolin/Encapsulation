833bb7c8d1735fb5e20ee3ca1e41ba89e2e8101f
Lavolin <lavolin@gmail.com>
2022-06-05 12:21:16 +0300

    05061221

---
+++ E03ShoppingSpree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E03ShoppingSpree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
