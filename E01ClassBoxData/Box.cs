3fa7428e69329df53e289f4fc75dd7975b258aca
Lavolin <lavolin@gmail.com>
2022-06-07 19:24:06 +0300

    07061923

---
+++ E01ClassBoxData/Box.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+
+namespace E01ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            //Surface Area = 2lw + 2lh + 2wh
+            return 2 * length * width + 2 * length* height + 2 * width * height;
+            
+        }
+
+        public double LateralSurfaceArea()
+        {
+            //Lateral Surface Area = 2lh + 2wh
+            return 2 * length * height + 2 * width * height;
+            
+        }
+
+        public double Volume()
+        {
+            //Volume = lwh  
+            return length * width * height;
+            
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Surface Area - {SurfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {LateralSurfaceArea():f2}");
+            sb.AppendLine($"Volume - {Volume():f2}");
+            
+
+            return sb.ToString().Trim();
+        }
+
+    }
+}
