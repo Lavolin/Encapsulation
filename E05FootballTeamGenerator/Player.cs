68f442d431798c47e2e837787d14f8ea7fc8ab77
Lavolin <lavolin@gmail.com>
2022-06-07 15:36:43 +0300

    07061536

---
+++ E05FootballTeamGenerator/Player.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E05FootballTeamGenerator
+{
+    public class Player
+    {
+        private string name;
+        private int endurance;
+        private int sprint;
+        private int dribble;
+        private int passing;
+        private int shooting;
+        public Player(string name, int endurance,
+            int sprint, int dribble,int passing, int shooting)
+        {
+            Name = name;
+
+            Validate("Endurance", endurance);
+            Validate("Sprint", sprint);
+            Validate("Dribble", dribble);
+            Validate("Passing", passing);
+            Validate("Shooting", shooting);
+
+            this.endurance = endurance;
+            this.sprint = sprint;
+            this.dribble = dribble;
+            this.passing = passing;
+            this.shooting = shooting;
+        }
+
+        
+
+        public string Name
+        {
+            get { return name; }
+
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+                name = value; 
+            }
+        }
+
+        public double Stats
+            => (this.endurance + this.sprint + this.dribble + this.passing + this.shooting) 
+            / 5.0;
+
+        private void Validate(string name ,int endurance)
+        {
+            if (endurance < 0 || endurance > 100)
+            {
+                throw new ArgumentException($"{name} should be between 0 and 100.");
+            }
+        }
+    }
+}
---
+++ E05FootballTeamGenerator/Team.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace E05FootballTeamGenerator
+{
+    public class Team
+    {
+        private string name;
+        private List<Player> players;
+
+        public Team(string name)
+        {
+            Name = name;
+
+            this.players = new List<Player>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+                name = value;
+            }
+        }
+        
+        public void AddPlayer(Player player)
+                   => players.Add(player);
+
+        public bool RemovePlayer(string name)
+        {
+            Player player = players.FirstOrDefault(x => x.Name == name);
+            return players.Remove(player); // null = false, jhk = false, Todor = true
+        }
+
+        public int Stats
+            => players.Any()
+            ? (int)Math.Round(players.Average(x => x.Stats))
+            : 0;
+    }
+}
